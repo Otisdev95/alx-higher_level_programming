12460114fbb015e104936a24d49942d8edc6d4a2
otis <olaliotis95@gmail.com>
2023-04-04 07:52:58 -0700

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,6 +30,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+
         self.__height = value
 
     @property
@@ -44,4 +45,5 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+
         self.__width = value
