347589dbb5094d84bb71fb04668ceada7836efba
otis <olaliotis95@gmail.com>
2023-04-04 08:21:02 -0700

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -12,4 +12,19 @@ def init_board(n):
     return (board)
 
 
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
 
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    outcome = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                outcome.append([r, c])
+                break
+    return (outcome)
