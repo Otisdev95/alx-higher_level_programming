cf3dacb4acf41e3d6594fde35f91800347069c58
otis <olaliotis95@gmail.com>
2023-04-04 08:11:04 -0700

    0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -5,3 +5,11 @@ import sys
 
 
 def init_board(n):
+    """Initializes an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+
