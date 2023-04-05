f43316264138fedeecc54e0db301d7081f4b3691
otis <olaliotis95@gmail.com>
2023-04-04 22:18:41 -0700

    removed wrong 0x07-python-test_driven_development

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,7 +5,5 @@ def square_matrix_simple(matrix=[]):
 
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x ** 2, matrix[i]))
-        new_martix[i] = list(map(lambda y: y ** 2, matrix[i]))
-        new_martix[i] = list(map(lambda z: z ** 2, matrix[i]))
 
-        return (new_matrix[i])
+        return (new_matrix)
