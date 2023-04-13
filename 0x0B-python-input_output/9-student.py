90470d363a18d76f246dfd2ce63fbdce1c4ba8f3
otis <olaliotis95@gmail.com>
2023-04-12 18:58:52 -0700

    0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module with class student define by public attributes"""
+
+
+class student:
+    """ class that defines student by public attributes """
+
+    def __init__(self, first_name, last_name, age):
+        """ Def that initializes the public attributes """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ method that retrives the dictionary rep """
+        return self.__dict__
