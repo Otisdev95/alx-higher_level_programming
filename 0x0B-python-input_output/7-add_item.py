235264a97c0a14ec1c02ebc6a383c58535d2c818
otis <olaliotis95@gmail.com>
2023-04-12 18:39:09 -0700

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,14 +5,15 @@
 """
 
 import sys
+import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
     try:
-        arguments = load_from_json_file("add_item.json")
+        pythonList = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        arguments = []
-        arguments.extend(sys.argv[1:])
-        save_to_json_file(arguments, "add_item.json")
+        pythonList = []
+    pythonList.extend(sys.argv[1:])
+    save_to_json_file(pythonList, "add_item.json")
