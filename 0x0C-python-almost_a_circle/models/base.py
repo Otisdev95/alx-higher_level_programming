edd01c95c66c845c7f01e752fb3b1408670ac846
otis <olaliotis95@gmail.com>
2023-04-19 06:36:22 -0700

    0x0C. Python - Almost a circle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,11 +22,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """static method returning JSON representation"""
         if list_dictionaries is None or list_dictionaries == []:
-            return []
-
-        if (type(list_dictionaries) != list or not
-                all(type(i) == dict for i in list_dictionaries)):
-            raise TypeError("list_dictionaries is not a list of dictionaries")
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
