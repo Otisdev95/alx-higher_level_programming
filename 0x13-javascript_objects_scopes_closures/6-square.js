878068895458eaccd401396dffd71f861f09322f
otisdev95 <olaliotis95@gmail.com>
2023-06-15 07:37:26 +0100

    JavaScript - Objects, Scopes and Closures

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
-
 const OtherSquare = require('./5-square');
 
 class Square extends OtherSquare {
-    charPrint (c) {
-        if (c === undefined) {
-            this.print();
-        } else {
-            for (let a = 0; a < this.height; a++) console.log(c.repeat(this.width));
-        }
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let a = 0; a < this.height; a++) console.log(c.repeat(this.width));
     }
+  }
 }
 
 module.exports = Square;
