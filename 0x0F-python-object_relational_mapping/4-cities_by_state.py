a20fae43424597c6ae931a3c4204011fa697c14d
otis <olaliotis95@gmail.com>
2023-06-19 23:39:08 -0700

    Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Script that lists all cities
+    from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+        Accesses to the database and get the cities
+        from the database.
+    """
+
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                    FROM cities JOIN states ON cities.state_id = states.id
+                    ORDER BY cities.id ASC""")
+    rows = cursor.fetchall()
+
+    if rows is not None:
+        for row in rows:
+            print(row)
